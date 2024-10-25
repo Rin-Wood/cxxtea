585bc6b277d090fec362dedd5ba14c3587c68963
wood <153263099+Rin-Wood@users.noreply.github.com>
2024-10-25 16:10:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,20 @@
 from setuptools import setup, Extension
-from wheel.bdist_wheel import bdist_wheel
+import os
 
-class bdist_wheel_abi3(bdist_wheel):
-    def get_tag(self):
-        python, abi, plat = super().get_tag()
-
-        if python.startswith("cp"):
-            return "cp36", "abi3", plat
-
-        return python, abi, plat
-
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+extension = Extension('xxtea', ['xxtea.c'])
 
 setup(
-    name="cxxtea",
-    version="1.0.1",
+    name="xxtea",
+    version=1.0.2,
     author='wood',
     author_email='miraclerinwood@gmail.com',
     url='https://github.com/ifduyue/cxxtea',
     description="cxxtea is a simple block cipher implemented in C",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description=open('README.md', 'rb').read().decode('utf8'),
+	long_description_content_type='text/markdown',
     license="BSD",
-    ext_modules=[
-        Extension(
-            "cxxtea",
-            sources=["cxxtea.c"],
-            define_macros=[("Py_LIMITED_API", "0x03060000")],
-            py_limited_api=True,
-        )
-    ],
-    cmdclass={"bdist_wheel": bdist_wheel_abi3},
+    keywords="xxtea",
+    ext_modules=[extension],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
