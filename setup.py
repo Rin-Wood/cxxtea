4b0be72f7a87270038fdf660d0bc43dcb7a98bb9
wood <153263099+Rin-Wood@users.noreply.github.com>
2024-10-25 14:57:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ class bdist_wheel_abi3(bdist_wheel):
         python, abi, plat = super().get_tag()
 
         if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.6
             return "cp36", "abi3", plat
 
         return python, abi, plat
